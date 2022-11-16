881cc9f986a6ca6d4411261a612f771b97152ed5
Kheyyy <sokeykim@gmail.com>
2022-11-16 15:45:40 +0700

    insert data to phases/jobs

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -36,6 +36,7 @@ public class DemoApplication {
         phaseService.deleteAll();
         inspectorService.deleteAll();
         defectService.deleteAll();
+
         defectService.add("12","Youssef","Flaw 1 Desc","1","2","4","Low","10/19/2022","Yes");
         defectService.add("7","Anhelina","Flaw 2 Desc","1","3","2","High","10/17/2022","No");
         defectService.add("4","Arsalan","Flaw 3 Desc","1","1","6","High","10/22/2022","Yes");
@@ -44,6 +45,34 @@ public class DemoApplication {
         inspectorService.add("324","1607","23","78","2","67");
         inspectorService.add("561","2431","12","15","9","103");
         inspectorService.add("405","2604","41","66","7","81");
+
+        phaseService.add("authentication", "Allow users to register and login", "1", "4", "High", "11/02/2022","Waterfall", "1", "No");
+        phaseService.add("User Role", "Create user Role", "1", "4", "Low", "13/02/2022","Waterfall", "1", "No");
+        phaseService.add("Product", "Create product item", "1", "4", "Low", "21/11/2022","Waterfall", "1", "No");
+        phaseService.add("Menu", "Manage menus all menu in which other users can see", "1", "2", "Low", "22/11/2022","Waterfall", "1", "No");
+        phaseService.add("Web admin", "Web admin for manage data of the project such as products, user roles, .etc.", "1", "4", "Low", "23/11/2022","Waterfall", "1", "No");
+        phaseService.add("Inventory adjustment", "Inventory adjust to manage or track the products whether instock or not. Be able to create delete, update.", "1", "2", "Low", "24/12/2022","Agile", "1", "No");
+        phaseService.add("Purchase order", "Manage purchase order, able to create, update, delete purchase.", "1", "2", "Low", "24/12/2022","Agile", "1", "No");
+        phaseService.add("Purchase Payment", "Manage purchase payment", "1", "3", "Low", "25/12/2022","Agile", "1", "No");
+        phaseService.add("Payment", "Manage Payment, able to search payment id", "2", "1", "Low", "10/01/2023","Agile", "1", "No");
+        phaseService.add("Receipt", "Manage receipt, able to search, filter, create, update receipt", "1", "2", "Low", "12/01/2023","Agile", "1", "No");
+
+        jobService.add("Register", "Create register form", "1", "High", "10/11/2022", "1", "Yes");
+        jobService.add("Login", "Create Login function", "1", "Low", "12/11/2022", "1", "NO");
+        jobService.add("Login", "Edit Login function", "1", "Low", "13/11/2022", "1", "Yes");
+        jobService.add("Login", "Delete Login function", "1", "Low", "14/11/2022", "1", "Yes");
+        jobService.add("Role Form", "Create user role form", "2", "High", "16/11/2022", "2", "NO");
+        jobService.add("Role", "Create user role function", "2", "Low", "17/11/2022", "2", "N0");
+        jobService.add("Role", "Edit user role function", "2", "Low", "18/11/2022", "2", "NO");
+        jobService.add("Role", "Delete user role function", "2", "Low", "19/11/2022", "2", "NO");
+        jobService.add("Product form", "Create product form", "3", "Low", "21/11/2022", "3", "NO");
+        jobService.add("Create Product", "Create product function", "3", "Low", "22/11/2022", "3", "NO");
+        jobService.add("Update Product", "Update product function", "3", "Low", "23/11/2022", "3", "NO");
+        jobService.add("Edit Product", "Edit product function", "3", "Low", "23/11/2022", "3", "NO");
+        jobService.add("Delete Product", "Delete product function", "3", "Low", "24/11/2022", "3", "NO");
+        jobService.add("Product Filter", "Create product filter", "3", "Low", "25/11/2022", "3", "NO");
+        jobService.add("Product review", "Review all function of product", "4", "Low", "27/11/2022", "3", "NO");
+
     }
 
 }
